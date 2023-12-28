a258b369b8a5a3f007c0de4650528e3fa4da83bd
prazzdev <prazzid31@gmail.com>
2023-12-28 22:45:55 +0700

    modified:   src/components/OverlayMenuSection.jsx 	modified:   src/pages/peta-desa/index.jsx 	modified:   src/styles/globals.css

--- src/components/OverlayMenuSection.jsx
+++ src/components/OverlayMenuSection.jsx
@@ -3,30 +3,30 @@ import Image from "next/image";
 export default function OverlayMenuSection() {
     return (
         <>
-        <section id="overlay-menu" className="max-w-[85vw] flex flex-row justify-around mx-auto -mt-[5em] px-[7em] py-9 bg-[#D9D9D9]">
-          <div className="menu flex flex-col justify-center items-center">
+        <section id="overlay-menu" className="max-w-[85vw] flex flex-row justify-around mx-auto -mt-[5em] px-[7em] py-9 bg-gray-200 shadow-lg">
+          <div className="menu flex flex-col justify-center items-center min-w-[250px] py-6 px-[3.5em] bg-gray-300 hover:bg-gray-400 hover:shadow-xl transition-all rounded-xl">
             <figure className={`bg-blue-400 p-6 rounded-full`}>
                 <Image 
                     src={'/assets/images/layanan-mandiri-icon.svg'} width={50} height={50} />
             </figure>
             <h3 className="font-inter-bold uppercase text-md text-gray-900 my-4">Layanan Mandiri</h3>
-            <button className={`bg-blue-400 font-inter-bold text-md text-white py-1 px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
+            <button className={`bg-blue-400 hover:bg-blue-500 transition-all font-inter-bold text-md text-white py-1 px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
           </div>
-          <div className="menu flex flex-col justify-center items-center">
+          <div className="menu flex flex-col justify-center items-center min-w-[250px] py-6 px-[3.5em] bg-gray-300 hover:bg-gray-400 hover:shadow-xl transition-all rounded-xl">
             <figure className={`bg-green-400 p-6 rounded-full`}>
                 <Image 
                     src={'/assets/images/lapak-icon.svg'} width={50} height={50} />
             </figure>
             <h3 className="font-inter-bold uppercase text-md text-gray-900 my-4">Lapak</h3>
-            <button className={`bg-green-400 font-inter-bold text-md text-white py-1 px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
+            <button className={`bg-green-400 hover:bg-green-500 font-inter-bold text-md text-white py-1 px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
           </div>
-          <div className="menu flex flex-col justify-center items-center">
+          <div className="menu flex flex-col justify-center items-center min-w-[250px] py-6 px-[3.5em] bg-gray-300 hover:bg-gray-400 hover:shadow-xl transition-all rounded-xl">
             <figure className={`bg-orange-400 p-6 rounded-full`}>
                 <Image 
                     src={'/assets/images/produk-hukum-icon.svg'} width={50} height={50} />
             </figure>
             <h3 className="font-inter-bold uppercase text-md text-gray-900 my-4">Produk Hukum</h3>
-            <button className={`bg-orange-400 font-inter-bold text-md text-white py-1 px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
+            <button className={`bg-orange-400 hover:bg-orange-500 font-inter-bold text-md text-white py-1 px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
           </div>
         </section>
         </>
--- src/pages/peta-desa/index.jsx
+++ src/pages/peta-desa/index.jsx
@@ -7,7 +7,7 @@ const Index = () => {
         <>
         <MainLayout title="Peta Desa">
             <Section title="Peta Desa">
-                <div className="w-full flex flex-col lg:flex-row justify-between items-center mt-4">
+                <div className="w-full flex flex-col lg:flex-row justify-between items-center my-4">
                     {/* Aboveview */}
                     <iframe 
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63305.68695718417!2d109.61266776953124!3d-7.3980303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7aabdd63dc1525%3A0xcbf6e965188f343a!2sKantor%20Desa%20Pucang!5e0!3m2!1sid!2sid!4v1703774433835!5m2!1sid!2sid" 
