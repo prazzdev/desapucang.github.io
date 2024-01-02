9af25a03d26027714ff6b029a28f9b96e1774f95
prazzdev <prazzid31@gmail.com>
2024-01-02 11:16:08 +0700

    modified:   src/components/HeroSection.jsx 	modified:   src/components/Navbar.jsx 	modified:   src/components/OverlayMenuSection.jsx 	modified:   src/styles/globals.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,23 +26,23 @@ export default function Navbar() {
     return (
         <>
         <nav className="flex flex-col bg-[#EEEEEE] overflow-x-hidden">
-            <div className="flex justify-between items-center my-9 px-[6em] bg-red-">
+            <div className="flex justify-between items-center lg:my-9 px-3 lg:px-[6em] py-3 lg:py-0 bg-red-">
                 <a 
                     href="/" id="logo" 
                     className="flex items-center">
-                    <figure className="mr-5">
+                    <figure className="mr-3 lg:mr-5">
                         <Image 
                             src={'./assets/images/banjarnegara-kab.svg'} width={50} height={50}
-                            className="" />
+                            className="w-[30px] lg:w-[50px]" />
                     </figure>
                     <div className="caption">
-                        <h1 className="font-inter-bold uppercase text-lg -mb-1">Desa Pucang</h1>
-                        <h4 className="font-inter-reg text-[14px] -mb-1">Kecamatan Bawang</h4>
-                        <h4 className="font-inter-reg text-[14px]">Kabupaten Banjarnegara</h4>
+                        <h1 className="font-inter-bold uppercase text-[.8em] lg:text-lg -mb-1">Desa Pucang</h1>
+                        <h4 className="font-inter-reg text-[.6em] lg:text-[14px] -mb-1">Kecamatan Bawang</h4>
+                        <h4 className="font-inter-reg text-[.6em] lg:text-[14px]">Kabupaten Banjarnegara</h4>
                     </div>
                 </a>
                 <div id="navmenu" className="flex flex-col items-end bg-blue-">
-                    <div className="flex items-center mb-5">
+                    <div className="hidden lg:flex items-center mb-5">
                         <Image src={'/assets/images/map.svg'} width={25} height={25} className="-mr-5" />
                         <NavMenu text={'Peta Desa'} target={'/peta-desa'} />
                         |
@@ -51,7 +51,7 @@ export default function Navbar() {
                             type="text" name="search" id="search" placeholder="Pencarian" 
                             className="font-inter-reg bg-transparent p-2 border-2 border-gray-700 rounded-lg" />
                     </div>
-                    <div className="flex flex-wrap items-center bg-red-">
+                    <div className="hidden lg:flex flex-wrap items-center bg-red-">
                         <NavMenu text={'Beranda'} target={'/'} />
                         <NavMenu 
                             text={'Pemerintahan Desa'} target={'#'} haveChild={true}
--- src/components/OverlayMenuSection.jsx
+++ src/components/OverlayMenuSection.jsx
@@ -3,30 +3,36 @@ import Image from "next/image";
 export default function OverlayMenuSection() {
     return (
         <>
-        <section id="overlay-menu" className="max-w-[85vw] flex flex-row justify-around mx-auto -mt-[5em] px-[7em] py-9 bg-gray-200 shadow-lg">
-          <div className="menu flex flex-col justify-center items-center min-w-[250px] py-6 px-[3.5em] bg-gray-300 hover:bg-gray-400 hover:shadow-xl transition-all rounded-xl">
-            <figure className={`bg-blue-400 p-6 rounded-full`}>
+        <section id="overlay-menu" className="max-w-fit lg:max-w-[85vw] flex flex-row justify-around mx-auto lg:-mt-[5em] px-[7em] pl-[21em] lg:pl-[7em] py-9 bg-gray-200 shadow-lg overflow-x-scroll">
+          <div className="menu flex flex-col justify-center items-center min-w-[190px] lg:min-w-[250px] py-6 px-5 lg:px-[3.5em] mx-3 lg:mx-0 bg-gray-300 hover:bg-gray-400 hover:shadow-xl transition-all rounded-xl">
+            <figure className={`bg-blue-400 p-5 lg:p-6 rounded-full`}>
                 <Image 
-                    src={'/assets/images/layanan-mandiri-icon.svg'} width={50} height={50} />
+                    src={'/assets/images/layanan-mandiri-icon.svg'} width={50} height={50} 
+                    className="w-[30px] lg:w-[50px]"
+                />
             </figure>
-            <h3 className="font-inter-bold uppercase text-md text-gray-900 my-4">Layanan Mandiri</h3>
-            <button className={`bg-blue-400 hover:bg-blue-500 transition-all font-inter-bold text-md text-white py-1 px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
+            <h3 className="font-inter-bold uppercase text-xs lg:text-md text-gray-900 my-4">Layanan Mandiri</h3>
+            <button className={`bg-blue-400 hover:bg-blue-500 transition-all font-inter-bold text-xs lg:text-md text-white py-1 px-5 lg:px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
           </div>
-          <div className="menu flex flex-col justify-center items-center min-w-[250px] py-6 px-[3.5em] bg-gray-300 hover:bg-gray-400 hover:shadow-xl transition-all rounded-xl">
-            <figure className={`bg-green-400 p-6 rounded-full`}>
+          <div className="menu flex flex-col justify-center items-center min-w-[190px] lg:min-w-[250px] py-6 px-5 lg:px-[3.5em] mx-3 lg:mx-0 bg-gray-300 hover:bg-gray-400 hover:shadow-xl transition-all rounded-xl">
+            <figure className={`bg-green-400 p-5 lg:p-6 rounded-full`}>
                 <Image 
-                    src={'/assets/images/lapak-icon.svg'} width={50} height={50} />
+                    src={'/assets/images/lapak-icon.svg'} width={50} height={50} 
+                    className="w-[30px] lg:w-[50px]"
+                />
             </figure>
-            <h3 className="font-inter-bold uppercase text-md text-gray-900 my-4">Lapak</h3>
-            <button className={`bg-green-400 hover:bg-green-500 font-inter-bold text-md text-white py-1 px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
+            <h3 className="font-inter-bold uppercase text-xs lg:text-md text-gray-900 my-4">Lapak</h3>
+            <button className={`bg-green-400 hover:bg-green-500 font-inter-bold text-xs lg:text-md text-white py-1 px-5 lg:px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
           </div>
-          <div className="menu flex flex-col justify-center items-center min-w-[250px] py-6 px-[3.5em] bg-gray-300 hover:bg-gray-400 hover:shadow-xl transition-all rounded-xl">
-            <figure className={`bg-orange-400 p-6 rounded-full`}>
+          <div className="menu flex flex-col justify-center items-center min-w-[190px] lg:min-w-[250px] py-6 px-5 lg:px-[3.5em] mx-3 lg:mx-0 lg:mr-0 bg-gray-300 hover:bg-gray-400 hover:shadow-xl transition-all rounded-xl">
+            <figure className={`bg-orange-400 p-5 lg:p-6 rounded-full`}>
                 <Image 
-                    src={'/assets/images/produk-hukum-icon.svg'} width={50} height={50} />
+                    src={'/assets/images/produk-hukum-icon.svg'} width={50} height={50} 
+                    className="w-[30px] lg:w-[50px]"
+                />
             </figure>
-            <h3 className="font-inter-bold uppercase text-md text-gray-900 my-4">Produk Hukum</h3>
-            <button className={`bg-orange-400 hover:bg-orange-500 font-inter-bold text-md text-white py-1 px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
+            <h3 className="font-inter-bold uppercase text-xs lg:text-md text-gray-900 my-4">Produk Hukum</h3>
+            <button className={`bg-orange-400 hover:bg-orange-500 font-inter-bold text-xs lg:text-md text-white py-1 px-5 lg:px-6 uppercase rounded-sm`} onClick={() => alert('Under Maintenance.')}>Login</button>
           </div>
         </section>
         </>
